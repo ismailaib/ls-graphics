134d069dec6ddd66d59b8601828ecc51a1d38bc3
ismailaib <ismailaitbouhmad@gmail.com>
2022-12-19 12:32:38 +0100

    add card

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,6 @@
-let menu = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbar');
-
-menu.onclick = () => {
-        menu.classList.toggle('bx-x');
-        navbar.classList.toggle('active');
-};
-
-window.onscroll = () => {
-        menu.classList.remove('bx-x');
-        navbar.classList.remove('active')
-}
+const sr = ScrollReveal ({
+  distance: '60px',
+  duration: 2500,
+  reset:true,
+})
+sr.reveal('.items',{delay:100, origin:'top'})
